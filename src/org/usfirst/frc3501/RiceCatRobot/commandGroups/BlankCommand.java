5345bb4eb2e034b93f2b8e12b8d992d15d7bf128
David Dobervich <david.dobervich@gmail.com>
2016-11-28 16:42:48 -0800

    add BlankCommand.java

---
+++ src/org/usfirst/frc3501/RiceCatRobot/commandGroups/BlankCommand.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc3501.RiceCatRobot.commandGroups;
+
+public class BlankCommand extends CommandGroup {
+
+  public BlankCommand() {
+    // this command group does nothing!
+  }
+}
